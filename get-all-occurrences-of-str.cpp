38060041ecffa60751d94e684410ac3184a7ddf5
cassi-storm <108718777+cassi-storm@users.noreply.github.com>
2024-01-16 13:00:06 +0530

    Update get-all-occurrences-of-str.cpp

--- get-all-occurrences-of-str.cpp
+++ get-all-occurrences-of-str.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 
 vector<int> get_all_occurrences(string text, string pattern) {
-	string s = pattern + "#" + text;
-	int n = s.size();
-	vector<int> v(s.size(), 0);
-	int ln = 0;
-	for (int i = 1; i < n; ++i) {
-		while (ln && s[i] != s[ln]) {
-			ln = v[ln - 1];
+    string s = pattern + "#" + text;
+    int n = s.size();
+    vector<int> v(s.size(), 0);
+    int ln = 0;
+    for (int i = 1; i < n; ++i) {
+        while (ln && s[i] != s[ln]) {
+            ln = v[ln - 1];
+        }
+        if (s[i] == s[ln]) {
+            ++ln;
 		}
-		if (s[i] == s[ln]) {
-			++ln;
-		}
-		v[i] = ln;
-	}
-	int m = pattern.size();
-	vector<int> occurs;
-	for (int i = 0; i < n; ++i) {
-		if (v[i] == m) {
-			occurs.push_back(i - 2 * m);
-		}
-	}
-	return occurs;
+        v[i] = ln;
+    }
+    int m = pattern.size();
+    vector<int> occurs;
+    for (int i = 0; i < n; ++i) {
+        if (v[i] == m) {
+            occurs.push_back(i - 2 * m);
+        }
+    }
+    return occurs;
 }
 
 int main() {
-	auto x = get_all_occurrences("sandyblazekksjdklasandykasjdlasandysandylklajssandy", "sandy");
-	for (auto i : x) {
-		cout << i << " ";
-	}
-	cout << "\nHello, World!\n";
+    auto x = get_all_occurrences("sandyblazekksjdklasandykasjdlasandysandylklajssandy", "sandy");
+    for (auto i : x) {
+        cout << i << " ";
+    }
+    cout << "\nHello, World!\n";
 }
