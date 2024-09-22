26f101a86c2c9635ade7e9d556820c1116cfdc8b
cassi-storm <108718777+cassi-storm@users.noreply.github.com>
2024-09-22 09:53:00 +0530

    Create pal-substr.py

---
+++ pal-substr.py
@@ -0,0 +1,13 @@
+n = 2 * len(s) + 1
+v = '^' + '#'.join(s) + '$'
+
+C, R = 0, 0
+P = [0] * n
+for i in range(1, n - 1):
+    M = 2 * C - i
+    if i < R:
+        P[i] = min(R - i, P[M])
+    while v[i + P[i] + 1] == v[i - P[i] - 1]:
+        P[i] += 1
+    if R < i + P[i]:
+        C, R = i, i + P[i]
